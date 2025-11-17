4204f3c226326e2fc33d5936d4661ea76ac0a9df
Joeri Exelmans <joeri.exelmans@gmail.com>
2025-11-17 17:26:34 +0100

    even beter plots

--- src/App/Plot/Plot.tsx
+++ src/App/Plot/Plot.tsx
@@ -64,23 +64,28 @@ export function Plot({traces, displayTime, ...rest}: {traces: {[name: string]: [
     "var(--plot-color-3)",
     "var(--plot-color-4)",
     "var(--plot-color-5)",
-  ]
+  ];
+
+  const markerEveryXMs = Math.max(250*2**Math.ceil(Math.log2(displayTime/1000/30/width*2000)), 250);
+  const labelEveryXMarkers = 2;
+
+  // console.log({markerEveryXMs, labelEveryXMarkers});
 
   const marks = [];
-  for (let i=0; i<displayTime; i+=100) {
+  for (let i=0; i<displayTime; i+=markerEveryXMs) {
     marks.push(i);
   }
 
   return <div className="statusBar">
     <details>
       <summary>plot</summary>
-      <svg style={{height: height+30}} ref={refSVG} viewBox={`0 0 ${width} ${height+30}`} {...rest}>
+      <svg style={{height: height+18}} ref={refSVG} viewBox={`0 0 ${width} ${height+18}`} {...rest}>
         {marks.map((m,i) => {
-          const x = i*100/maxTime*width;
+          const x = i*(markerEveryXMs)/maxTime*width;
           return <>
-            <line x1={x} x2={x} y1={0} y2={height} stroke="var(--separator-color)"/>
-            {i%5===0 &&
-            <text x={x} y={height+16} textAnchor="middle" style={{fill: 'var(--text-color)'}}>{m/1000}</text>
+            <line x1={x} x2={x} y1={0} y2={height+2} stroke="var(--separator-color)"/>
+            {i%labelEveryXMarkers===0 &&
+              <text x={x} y={height+16} textAnchor="middle" style={{fill: 'var(--text-color)'}}>{m/1000}</text>
             }
             </>;
         })}
