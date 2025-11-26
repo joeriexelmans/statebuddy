08d60fff69395208fc716ba7dafaa83d39268f98
Joeri Exelmans <joeri.exelmans@gmail.com>
2025-11-26 11:02:00 +0100

    fix grid

--- src/App/VisualEditor/Grid.tsx
+++ src/App/VisualEditor/Grid.tsx
@@ -3,8 +3,8 @@ import { GRID_CELL_SIZE } from "../parameters";
 export function Grid({width, height}: {width: number, height: number}) {
   return <>
     {Array.from({length: width/GRID_CELL_SIZE}).map((_,i) => 
-      <line key={'v'+i*GRID_CELL_SIZE} x1={i*GRID_CELL_SIZE} x2={i*GRID_CELL_SIZE} y1={0} y2={height} strokeWidth={0.5} stroke="rgba(0, 206, 154)" style={{pointerEvents: 'none'}}/>)}
+      <line key={'v'+i*GRID_CELL_SIZE} x1={(i+1)*GRID_CELL_SIZE} x2={(i+1)*GRID_CELL_SIZE} y1={0} y2={height} strokeWidth={0.5} stroke="rgba(0, 206, 154)" style={{pointerEvents: 'none'}}/>)}
     {Array.from({length: height/GRID_CELL_SIZE}).map((_,i) => 
-      <line key={'h'+i*GRID_CELL_SIZE} x1={0} x2={width} y1={i*GRID_CELL_SIZE} y2={i*GRID_CELL_SIZE} strokeWidth={0.5} stroke="rgb(0, 206, 154)" style={{pointerEvents: 'none'}}/>)}
+      <line key={'h'+i*GRID_CELL_SIZE} x1={0} x2={width} y1={(i+1)*GRID_CELL_SIZE} y2={(i+1)*GRID_CELL_SIZE} strokeWidth={0.5} stroke="rgb(0, 206, 154)" style={{pointerEvents: 'none'}}/>)}
   </>
 }
