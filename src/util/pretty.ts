407a250b9c8d455c27a7795ea641d4e5145edfaf
Joeri Exelmans <joeri.exelmans@gmail.com>
2026-03-26 23:29:30 +0100

    fix bug in prettyNumber function

--- src/util/pretty.ts
+++ src/util/pretty.ts
@@ -1,6 +1,6 @@
 export function prettyNumber(n: number): string {
   if (n >= 1000) {
-    return `${Math.floor(n / 1000)},${n % 1000}`;
+    return `${Math.floor(n / 1000)},${(n % 1000).toString().padStart(3, '0')}`;
   }
   return n.toString();
 }
